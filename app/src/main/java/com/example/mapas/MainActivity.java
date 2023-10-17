ca9b4d121088e141788d7061ac89751228bbf70d
luuicdx9 <luiscdx9@gmail.com>
2023-10-17 16:39:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mapas/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mapas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView mivideo = findViewById(R.id.videoView);
+
+        String videop = "android.resource://" +getPackageName()+"/" + R.raw.csgo;
+        Uri uri = Uri.parse(videop);
+        mivideo.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mivideo.setMediaController(mediaController);
+        mediaController.setAnchorView(mivideo);
+
+
+
+
+    }
+}
